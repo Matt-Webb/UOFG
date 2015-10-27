4ba3421ada5bc40e0eb1bf280191aed791156a60
Matthew Webb <rybka.webb@gmail.com>
2015-10-27 15:01:57 +0000

    attempted to correct css & js ordering

--- client/config/router.js
+++ client/config/router.js
@@ -6,8 +6,12 @@ Router.configure({
 
 Router.route('/', function () {
     this.render('home');
+    $('body').addClass('color-green');
+    this.next();
 });
 
 Router.route('/schedule', function () {
   this.render('schedule');
+  $('body').addClass('color-green');
+  this.next();
 });
